60d5c24602071d4b7a9e97555682e6378cf8d975
@_nikhil_more <nickmore1999@gmail.com>
2022-11-29 14:14:20 +0530

    first commit

--- next-nft/pages/api/[tokenId].js
+++ next-nft/pages/api/[tokenId].js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
-  const img = `https://github.com/LearnWeb3DAO/NFT-Collection/blob/main/my-app/public/cryptodevs/${tokenId}-1.svg`;
+  const img = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${tokenId}.svg`;
   const name = "cryptoDev by Nikhil";
   const description = "created for project purpose only";
 
