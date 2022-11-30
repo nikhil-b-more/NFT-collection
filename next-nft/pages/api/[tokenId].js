b7f72fb6bf823acf8ea66ca664d5f7f486bb1d7f
@_nikhil_more <nickmore1999@gmail.com>
2022-11-30 14:30:24 +0530

    first

--- next-nft/pages/api/[tokenId].js
+++ next-nft/pages/api/[tokenId].js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
-  const img = `https://ipfs.io/ipfs/QmYnn5iZQqTeusPq2jnj7Y1uapxdMwtG3cRfP1LfGQcqn5?filename=ms-shr.png`;
+  const img = `https://ipfs.io/ipfs/QmYnn5iZQqTeusPq2jnj7Y1uapxdMwtG3cRfP1LfGQcqn5?filename=ms-shr-`;
   const name = "cryptoDev by Nikhil";
   const description = "created for project purpose only";
 
